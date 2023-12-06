eeb1d98ee54e89e05bde92bdaa0ac0b152aeaa45
Daniel Messiana <40705754+DanielMessiana@users.noreply.github.com>
2023-12-06 18:54:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import streamlit as st
+import pandas as pd
+from io import StringIO
 import time
+st.title("Data Organizer")
 
 
-print("Hello, world!")
+uploaded_file = st.file_uploader("Choose a file")
+
+if uploaded_file is not None:
+
+	dataframe = pd.read_csv(uploaded_file)
