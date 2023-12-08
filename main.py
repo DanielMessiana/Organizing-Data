d1e1ba1e8b3714c24d81dec63a052f19ac1600b2
Daniel Messiana <40705754+DanielMessiana@users.noreply.github.com>
2023-12-08 14:54:53 -0500

    added kmeans
    -added kmeans clustering

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import matplotlib.pyplot as plt
 import streamlit as st
 import pandas as pd
 from io import StringIO
+from sklearn.metrics.pairwise import euclidean_distances
 import time
 
 
 st.sidebar.title("Models for Data")
 
-options = ['Home', 'Linear Regression']
+options = ['Home', 'Linear Regression', 'Kmeans']
 side_menu = st.sidebar.radio("Side menu widgets", options, label_visibility="hidden")
 
 #if side_menu
@@ -48,3 +49,88 @@ if side_menu == 'Linear Regression':
 		plt.plot(x, y, 'ok')
 		plt.plot(x, y_hat)
 		st.pyplot(plt)
+
+class kmeans:
+    """ The k-Means algorithm"""
+    
+    def __init__(self, k, data):
+        """Initially assign each sample to a random cluster.
+           Assign each centroid to be a zero vector.
+        """
+        self.nData = np.shape(data)[0]
+        self.nDim = np.shape(data)[1]
+        self.k = k
+        self.data = np.hstack((np.random.randint(0, self.k, (self.nData, 1)), data))
+        self.centroids = np.zeros((self.k, self.nDim))
+        self.make_centroids()
+        self.wcv = np.zeros(self.k)
+
+    def make_centroids(self):
+        """Calculate centroids of current clusters"""
+        
+        for i in range(self.k):
+            cluster = self.data[self.data[:, 0]==i]
+            cluster = cluster[:, 1:]
+            self.centroids[i] = np.mean(cluster, axis=0)
+
+    def make_clusters(self):
+        distances = np.zeros((self.nData, self.k))
+        for i in range(self.k):
+            distances[:, i] = np.sum((self.data[:, 1:] - self.centroids[i,:])**2,axis=1)
+        self.data[:,0] = np.argmin(distances, axis=1)
+        
+    def within_cluster_variation(self):
+        """Calculate within-cluster variation of current clusters"""
+        
+        for i in range(self.k):
+            cluster = self.data[self.data[:, 0]==i]
+            cluster = cluster[:, 1:]
+            distances = euclidean_distances(cluster, cluster)
+            self.wcv[i] = np.sum(distances)/(2*cluster.shape[0])
+        
+    def cluster(self, iterations=1):
+        """Perform k-Means"""
+        for i in range(iterations):
+            self.make_clusters()
+            self.make_centroids()
+        self.within_cluster_variation()
+        labels = self.data[:,0]
+        return labels, self.centroids
+
+if side_menu == 'Kmeans':
+	st.title('Kmeans Clustering')
+
+	if 'data' not in st.session_state:
+		"Couldn't find data, please try again."
+	else:
+		data = st.session_state['data']
+		features = list(data.columns)
+
+		x_label = st.selectbox('Set x label', features)
+		y_label = st.selectbox('Set y label', features)
+		x = data[x_label]
+		y = data[y_label]
+		
+		features = np.vstack([[x], [y]]).T
+
+		c = st.number_input("Number of Classes", value=3)
+
+		km = kmeans(c, features)
+		labels, centroids = km.cluster(10)
+		c0 = data[labels==0]
+		c1 = data[labels==1]
+		c2 = data[labels==2]
+
+		plt.plot(c0[x_label], c0[y_label], 'ob', alpha=0.2)
+		plt.plot(c1[x_label], c1[y_label], 'or', alpha=0.2)
+		plt.plot(c2[x_label], c2[y_label], 'og', alpha=0.2)
+		plt.plot(centroids[0,0], centroids[0,1], 'ob')
+		plt.plot(centroids[1,0], centroids[1,1], 'or')
+		plt.plot(centroids[2,0], centroids[2,1], 'og')
+		plt.title("Initial clusters")
+		plt.xlabel(x_label)
+		plt.ylabel(y_label)
+		st.pyplot(plt)
+
+
+
