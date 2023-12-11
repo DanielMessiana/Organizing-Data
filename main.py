96a22052dae5f7a76bbd53ecff166b2c4fd634ac
Daniel Messiana <40705754+DanielMessiana@users.noreply.github.com>
2023-12-11 13:11:32 -0500

    small changes

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ import streamlit as st
 import pandas as pd
 from io import StringIO
 from sklearn.metrics.pairwise import euclidean_distances
+from sklearn.linear_model import LogisticRegression
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn import preprocessing 
 import time
 
 st.sidebar.title("Models for Data")
 
-options = ['Home', 'Linear Regression', 'Kmeans']
+options = ['Home', 'Linear Regression', 'Logistic Regression', 'Kmeans']
 side_menu = st.sidebar.radio("Side menu widgets", options, label_visibility="hidden")
 
 # Home Page
@@ -23,6 +28,16 @@ if side_menu == 'Home':
 	if uploaded_file is not None:
 
 		dataframe = pd.read_csv(uploaded_file)
+
+
+		label_encoder = preprocessing.LabelEncoder()
+		st.header("Unique String Values")
+		for column in dataframe.columns:
+			
+			if type(pd.unique(dataframe[column])[0]) == str:
+				f"{column.capitalize()} has {pd.unique(dataframe[column]).shape[0]}."
+				f"Classes (strings): {pd.unique(dataframe[column])}"
+			dataframe[column]= label_encoder.fit_transform(dataframe[column]) 
 		
 		st.session_state['data'] = dataframe	
 	if 'data' not in st.session_state:
@@ -39,24 +54,78 @@ if side_menu == 'Linear Regression':
 	st.title('Linear Regression')
 
 	if 'data' not in st.session_state:
-		"Couldn't find data, please try again."
+		"Couldn't find data, please upload a dataset."
 	else:
 		data = st.session_state['data']
 		features = list(data.columns)
 
-		x = data[st.selectbox('Set x label', features)]
-		y = data[st.selectbox('Set y label', features)]
-
+		x_label = st.selectbox('Set x label', features)
+		y_label = st.selectbox('Set y label', features)
+		x = data[x_label]
+		y = data[y_label]
 
 		ones = np.ones(data.shape[0])
 		X = np.vstack([[x], [ones]]).T
 		
 		beta_hat = np.linalg.pinv(X.T @ X + 0.001 * np.eye(X.shape[1])) @ X.T @ y.T
-
 		y_hat = np.dot(X, beta_hat)
 
-		plt.plot(x, y, 'ok')
+		plt.plot(x, y, 'ok', alpha=0.6)
 		plt.plot(x, y_hat)
+		plt.xlabel(x_label)
+		plt.ylabel(y_label)
+		st.pyplot(plt)
+
+if side_menu == 'Logistic Regression':
+	st.title('Logistic Regression')
+
+	if 'data' not in st.session_state:
+		"Couldn't find data, please upload a dataset."
+	else:
+		data = st.session_state['data']
+		features = list(data.columns)
+
+		x_label = st.selectbox('Set x label', features)
+		y_label = st.selectbox('Set y label', features)
+		x = data[x_label]
+		y = data[y_label].astype(float)
+
+		ones = np.ones(data.shape[0])
+		X = np.vstack([x, ones]).T
+
+		beta = np.zeros(X.shape[1])
+
+		def sig(x):
+			return 1 / (1 + np.exp(-x))
+
+		def cost_function(X, y, beta):
+			z = np.dot(X, beta)
+			p = sig(z)
+			return -np.mean(y * np.log(p) + (1 - y) * np.log(1 - p))
+
+		def gradient(X, y, beta):
+			z = np.dot(X, beta)
+			p = sig(z)
+			return np.dot(X.T, (p - y)) / len(y)
+
+		learning_rate = 0.01
+		num_iterations = 1000
+
+		for i in range(num_iterations):
+			beta -= learning_rate * gradient(X, y, beta)
+
+		probabilities = sig(np.dot(X, beta))
+
+
+		sorted_indices = np.argsort(x)
+		x_sorted = x.iloc[sorted_indices]
+		probabilities_sorted = probabilities[sorted_indices]
+
+		plt.scatter(x, y, color='black', marker='o', label='Actual')
+		plt.plot(x_sorted, probabilities_sorted, color='blue', label='Predicted Probabilities')
+		plt.xlabel(x_label)
+		plt.ylabel(y_label)
+		plt.legend()
 		st.pyplot(plt)
 
 # Kmeans Class
@@ -114,7 +183,7 @@ if side_menu == 'Kmeans':
 	st.title('Kmeans Clustering')
 
 	if 'data' not in st.session_state:
-		"Couldn't find data, please try again."
+		"Couldn't find data, please upload a dataset."
 	else:
 		data = st.session_state['data']
 		features = list(data.columns)
@@ -123,8 +192,6 @@ if side_menu == 'Kmeans':
 		y_label = st.selectbox('Set y label', features)
 		x = data[x_label]
 		y = data[y_label]
-		if x.dtype == "object":
-
 		
 		x_u = pd.unique(x)
 		y_u = pd.unique(y)
@@ -135,7 +202,7 @@ if side_menu == 'Kmeans':
 		c = st.number_input("Number of Classes", value=3)
 
 		km = kmeans(c, features)
-		labels, centroids = km.cluster(5)
+		labels, centroids = km.cluster(8)
 		c0 = data[labels==0]
 		c1 = data[labels==1]
 		c2 = data[labels==2]
@@ -149,6 +216,25 @@ if side_menu == 'Kmeans':
 		plt.xlabel(x_label)
 		plt.ylabel(y_label)
 		st.pyplot(plt)
+
+if side_menu == 'Neural Network':
+	st.title('Neural Network')
+
+	if 'data' not in st.session_state:
+		"Couldn't find data, please upload a dataset."
+	else:
+		data = st.session_state['data']
+		features = list(data.columns)
+
+
+
+
+
+
+
+
+
+
 
 
 
